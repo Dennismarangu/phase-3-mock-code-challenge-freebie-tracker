2b8a3356d494d62d43c6a2d89d155444b0d4fb28
Dennismarangu <dennismutumamarangu@gmail.com>
2023-06-04 12:36:52 +0300

    app/models

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,12 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
 
+  def received_one?(item_name)
+    freebies.exists?(item_name: item_name)
+  end
+
+  def give_away(dev, freebie)
+    freebie.update(dev: dev)
+  end
 end
--- app/models/freebie.rb
+++
@@ -1,3 +0,0 @@
-class Freebie < ActiveRecord::Base
-
-end
