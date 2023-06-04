9f635fc1d98e596272aba3417af441e618d67b42
Dennismarangu <dennismutumamarangu@gmail.com>
2023-06-04 12:39:09 +0300

    create_freebies.rb

--- db/migrate/20230604093347_create_freebies.rb
+++ db/migrate/20230604093347_create_freebies.rb
@@ -1,13 +1,2 @@
-class CreateFreebies < ActiveRecord::Migration[6.0]
-  def change
-    create_table :freebies do |t|
-      t.string :item_name
-      t.integer :value
-      t.belongs_to :dev, foreign_key: true
-      t.belongs_to :company, foreign_key: true
 
-      t.timestamps
-    end
-  end
-end
 
